d09ce908034d09c6bf39ec4183b66ec8d8acaeb8
Thomas Heinrichs <52525.thomas@gmail.com>
2023-03-10 12:35:41 +0100

    Refactor: adding the other examples from the miranum repo (#2)
    * refactor: adding the other examples from the miranum repo
    
    * fix: remove gateway-example
    
    * refactor: application.properties to application.yaml
    
    * fix: Implemented review feedback

---
+++ process-example/process-example-core/src/main/java/io/miragon/miranum/examples/process/application/port/in/SendMessageCommand.java
@@ -0,0 +1,13 @@
+package io.miragon.miranum.examples.process.application.port.in;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class SendMessageCommand {
+    private String name;
+    private String key;
+}
