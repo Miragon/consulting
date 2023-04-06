40b30912d860df852a5a181f33bc67221a90b178
Andreas Riepl <andreas.riepl@miragon.io>
2023-04-06 11:32:22 +0200

    feat: local camunda setup

--- restaurant-showcase/order-restaurant/src/main/java/restaurant/showcase/order/OrderApplication.java
+++ restaurant-showcase/order-restaurant/src/main/java/restaurant/showcase/order/OrderApplication.java
@@ -1,11 +1,13 @@
 package restaurant.showcase.order;
 
+import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CheckoutApplication {
+@EnableZeebeClient
+public class OrderApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CheckoutApplication.class, args);
+        SpringApplication.run(OrderApplication.class, args);
     }
 }
