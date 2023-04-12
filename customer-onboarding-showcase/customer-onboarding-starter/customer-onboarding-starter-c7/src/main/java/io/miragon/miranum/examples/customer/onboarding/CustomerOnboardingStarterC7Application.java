1f7c46cfd422ead41958564fe5d2c53604de52b2
Hafflgav <52525.thomas@gmail.com>
2023-04-12 08:19:37 +0200

    fix: Optimize imports

--- customer-onboarding-showcase/customer-onboarding-starter/customer-onboarding-starter-c7/src/main/java/io/miragon/miranum/examples/customer/onboarding/CustomerOnboardingStarterC7Application.java
+++ customer-onboarding-showcase/customer-onboarding-starter/customer-onboarding-starter-c7/src/main/java/io/miragon/miranum/examples/customer/onboarding/CustomerOnboardingStarterC7Application.java
@@ -2,7 +2,6 @@ package io.miragon.miranum.examples.customer.onboarding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class CustomerOnboardingStarterC7Application {
--- customer-onboarding-showcase/customer-onboarding-starter/customer-onboarding-starter-core/src/main/java/io/miragon/miranum/examples/customer/onboarding/adapter/in/rest/dtos/PlaceApplicationResponseDto.java
+++ customer-onboarding-showcase/customer-onboarding-starter/customer-onboarding-starter-core/src/main/java/io/miragon/miranum/examples/customer/onboarding/adapter/in/rest/dtos/PlaceApplicationResponseDto.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
