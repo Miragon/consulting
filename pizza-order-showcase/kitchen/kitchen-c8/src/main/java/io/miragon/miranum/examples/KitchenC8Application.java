0b1f155b587c1d9f293a2c6fcca557de0433f402
Rami Nakkar <rami.nakkar@miragon.io>
2023-04-18 13:31:23 +0200

    feat: add data

--- pizza-order-showcase/kitchen/kitchen-c8/src/main/java/io/miragon/miranum/examples/KitchenC8Application.java
+++ pizza-order-showcase/kitchen/kitchen-c8/src/main/java/io/miragon/miranum/examples/KitchenC8Application.java
@@ -1,8 +1,8 @@
 package io.miragon.miranum.examples;
 
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableZeebeClient
