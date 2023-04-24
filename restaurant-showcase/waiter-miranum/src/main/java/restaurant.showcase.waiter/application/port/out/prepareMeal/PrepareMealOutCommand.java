c23e1c7a1bceb089c85dc0cf74c776aa630888db
Rami Nakkar <rami.nakkar@miragon.io>
2023-04-24 13:46:52 +0200

    fix: add JsonAutoDetect back

--- restaurant-showcase/waiter-miranum/src/main/java/restaurant.showcase.waiter/application/port/out/prepareMeal/PrepareMealOutCommand.java
+++ restaurant-showcase/waiter-miranum/src/main/java/restaurant.showcase.waiter/application/port/out/prepareMeal/PrepareMealOutCommand.java
@@ -1,9 +1,11 @@
 package restaurant.showcase.waiter.application.port.out.prepareMeal;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.RequiredArgsConstructor;
 import restaurant.showcase.waiter.domain.Order;
 
 @RequiredArgsConstructor
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class PrepareMealOutCommand {
 
     private final String orderId;
