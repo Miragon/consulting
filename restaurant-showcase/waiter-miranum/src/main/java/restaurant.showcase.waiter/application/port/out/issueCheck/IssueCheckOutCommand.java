99e129ff4ecfa2d84ac18219b1d79f8a9b48e8fc
Rami Nakkar <rami.nakkar@miragon.io>
2023-04-24 13:22:51 +0200

    refactor: remove @JsonAutoDetect

--- restaurant-showcase/waiter-miranum/src/main/java/restaurant.showcase.waiter/application/port/out/issueCheck/IssueCheckOutCommand.java
+++ restaurant-showcase/waiter-miranum/src/main/java/restaurant.showcase.waiter/application/port/out/issueCheck/IssueCheckOutCommand.java
@@ -1,12 +1,11 @@
 package restaurant.showcase.waiter.application.port.out.issueCheck;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.RequiredArgsConstructor;
 import restaurant.showcase.waiter.domain.Order;
 
 @RequiredArgsConstructor
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class IssueCheckOutCommand {
+
     private final String orderId;
     private final String customerName;
     private final String meal;
