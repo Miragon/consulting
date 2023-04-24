d1ae953dcd8b0b95c62dc6f49d01f84b7485fccb
Rami Nakkar <rami.nakkar@miragon.io>
2023-04-24 12:55:37 +0200

    fix: remove deprecated @EnableZeebeClient + autodeploy bpmn

--- restaurant-showcase/satisfaction-miranum/src/main/java/restaurant/showcase/satisfaction/SatisfactionApplication.java
+++ restaurant-showcase/satisfaction-miranum/src/main/java/restaurant/showcase/satisfaction/SatisfactionApplication.java
@@ -1,11 +1,9 @@
 package restaurant.showcase.satisfaction;
 
-import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableZeebeClient
 public class SatisfactionApplication {
     public static void main(String[] args) {
         SpringApplication.run(SatisfactionApplication.class, args);
--- restaurant-showcase/waiter-plain/src/main/java/restaurant/showcase/waiter/WaiterPlainApplication.java
+++ restaurant-showcase/waiter-plain/src/main/java/restaurant/showcase/waiter/WaiterPlainApplication.java
@@ -1,11 +1,11 @@
 package restaurant.showcase.waiter;
 
-import io.camunda.zeebe.spring.client.EnableZeebeClient;
+import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableZeebeClient
+@Deployment(resources = "classpath*:**/restaurant-plain.bpmn")
 public class WaiterPlainApplication {
     public static void main(String[] args) {
         SpringApplication.run(WaiterPlainApplication.class, args);
