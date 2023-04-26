3eae37791fd218b4343059a406574d6a661b353f
Hafflgav <52525.thomas@gmail.com>
2023-04-26 12:49:30 +0200

    style: adding deployment of process to SatisfactionApplication

--- restaurant-showcase/satisfaction-miranum/src/main/java/restaurant/showcase/satisfaction/SatisfactionApplication.java
+++ restaurant-showcase/satisfaction-miranum/src/main/java/restaurant/showcase/satisfaction/SatisfactionApplication.java
@@ -1,9 +1,11 @@
 package restaurant.showcase.satisfaction;
 
+import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Deployment(resources = "classpath*:**/satisfaction-miranum.bpmn")
 public class SatisfactionApplication {
     public static void main(String[] args) {
         SpringApplication.run(SatisfactionApplication.class, args);
