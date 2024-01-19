cba5a585bb8cbccce2b7738f9c7021fdc08030de
Hafflgav <52525.thomas@gmail.com>
2024-01-19 12:08:21 +0100

    feat: adding Pojo's

--- futue-proof-connector/notify-customer-connector/src/main/java/miranum/notification/customer/domain/Customer.java
+++ futue-proof-connector/notify-customer-connector/src/main/java/miranum/notification/customer/domain/Customer.java
@@ -1,4 +1,11 @@
 package miranum.notification.customer.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Customer {
+    private final String name;
+    private final String number;
 }
--- futue-proof-connector/notify-customer-connector/src/main/java/miranum/notification/customer/domain/Notification.java
+++ futue-proof-connector/notify-customer-connector/src/main/java/miranum/notification/customer/domain/Notification.java
@@ -1,4 +1,13 @@
 package miranum.notification.customer.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Notification {
+    private final String notificationMethod;
+    private final String topic;
+    private final String message;
+    private final Customer customer;
 }
