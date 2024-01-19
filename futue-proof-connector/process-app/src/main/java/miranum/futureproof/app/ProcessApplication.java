d0d056bd99c3a4646ab6e929231f00f384085536
Hafflgav <52525.thomas@gmail.com>
2024-01-19 14:28:04 +0100

    feat: adding process-app example

---
+++ futue-proof-connector/process-app/src/main/java/miranum/futureproof/app/ProcessApplication.java
@@ -0,0 +1,28 @@
+package miranum.futureproof.app;
+
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.spring.client.EnableZeebeClient;
+import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.*;
+
+@SpringBootApplication
+@EnableZeebeClient
+public class ProcessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessApplication.class, args);
+    }
+
+    @JobWorker(type = "getCustomer_NotificationPreference")
+    public Map<String, Object> getCustomer_NotificationPreference(final ActivatedJob job) {
+        List<String> givenList = Arrays.asList("SMS", "EMAIL");
+        Map<String, Object> variables = new HashMap<>();
+        Random rand = new Random();
+        String randomElement = givenList.get(rand.nextInt(givenList.size()));
+
+        variables.put("notificationMethod", randomElement);
+        return variables;
+    }
+}
